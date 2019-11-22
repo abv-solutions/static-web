cd5b6aeebb7bf85c7569356715b142554a83061d
Andrei Suciu <andreisuciu2005@yahoo.com>
2019-11-22 15:38:59 +0200

    Updates

---
+++ script.js
@@ -0,0 +1,7 @@
+function sum()
+{
+  var num1 = parseFloat(document.getElementById("firstNumber").value);
+  var num2 = parseFloat(document.getElementById("secondNumber").value);
+  var result = num1 + num2;
+  alert("Result: " + result);
+}
